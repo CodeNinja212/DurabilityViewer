3f43a7e94f2edbb3d0786349f4b943970bed7b1c
Ezio Hikari <34973433+CodeNinja212@users.noreply.github.com>
2023-12-16 17:07:50 +0800

    Add middle option part 1: Corner.java

--- src/main/java/de/guntram/mcmod/durabilityviewer/client/gui/Corner.java
+++ src/main/java/de/guntram/mcmod/durabilityviewer/client/gui/Corner.java
@@ -5,10 +5,13 @@ public enum Corner
     BOTTOM_RIGHT, 
     BOTTOM_LEFT, 
     TOP_RIGHT, 
-    TOP_LEFT;
+    TOP_LEFT,
+    MIDDLE_RIGHT,
+    MIDDLE_LEFT;
     
     public boolean isLeft() {return this==TOP_LEFT || this==BOTTOM_LEFT;}
     public boolean isRight() {return this==TOP_RIGHT || this==BOTTOM_RIGHT;}
-    public boolean isTop() { return this==TOP_LEFT || this==TOP_RIGHT; }
-    public boolean isBottom() { return this==BOTTOM_LEFT || this==BOTTOM_RIGHT; }
+    public boolean isTop() {return this==TOP_LEFT || this==TOP_RIGHT;}
+    public boolean isBottom() {return this==BOTTOM_LEFT || this==BOTTOM_RIGHT;}
+    public boolean isMiddle() {return this==MIDDLE_RIGHT || this==MIDDLE_LEFT;}
 }
